8503ea07bc5871ddecd67826ecb6d5bd3f4b7e87
Koudura Ninci <KouduraNinci@gmail.com>
2017-09-19 18:26:44 +0100

    1st sync : get readme.md [fill enum.cs]

---
+++ src/True.Fornax/Fornax.Net/enum.cs
@@ -0,0 +1,102 @@
+﻿/** MIT LICENSE
+* Copyright (c) 2017 Koudura Ninci @True.Inc
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+**/
+
+#region Query Enumerations 
+/// <summary>
+/// 
+/// </summary>
+public enum SearchMode { }
+
+/// <summary>
+/// 
+/// </summary>
+public enum QueryMode { }
+
+/// <summary>
+/// 
+/// </summary>
+public enum QueryType { }
+
+/// <summary>
+/// 
+/// </summary>
+public enum Expand { }
+
+/// <summary>
+/// 
+/// </summary>
+public enum FieldScope { }
+
+/// <summary>
+/// 
+/// </summary>
+public enum Zone { }
+#endregion
+
+#region IO Enumerations 
+/// <summary>
+/// 
+/// </summary>
+public enum CachingMode {}
+
+/// <summary>
+/// 
+/// </summary>
+public enum FetchAttribute {}
+
+/// <summary>
+/// 
+/// </summary>
+public enum TraversalMode { }
+#endregion
+
+#region Fornax Enumerations 
+/// <summary>
+/// 
+/// </summary>
+public enum FornaxFormat { }
+
+/// <summary>
+/// 
+/// </summary>
+public enum FornaxDependencies {}
+#endregion
+
+#region Index Enumerations
+/// <summary>
+/// 
+/// </summary>
+public enum IndexMode { }
+#endregion
+
+#region  Misc Enumerations
+/// <summary>
+/// 
+/// </summary>
+public enum FileExtension { }
+
+/// <summary>
+/// 
+/// </summary>
+public enum SortMode { }
+#endregion
